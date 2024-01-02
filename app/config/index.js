a1911056c96836cbf6c659c9826d5607687fe310
ThuTran <ThuTran>
2024-01-02 16:31:06 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 const config = {
     app: {
-        port: process.env.PORT || 3000,
+        port: 3000,
+    },
+    db: {
+        uri: "mongodb://127.0.0.1:27017/contactbook"
     }
 };
 module.exports = config;
